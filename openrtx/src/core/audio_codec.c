c3ed8e0af7068eb046596d5b1b7f970fec31c3a2
turbo2ltr <github@g-forcemotorsports.com>
2022-07-16 09:00:04 -0700

    This is a good clean volume control with average-based gate

--- openrtx/src/core/audio_codec.c
+++ openrtx/src/core/audio_codec.c
@@ -297,7 +297,6 @@ static void *decodeFunc(void *arg)
             // most basic volume control
             
             int16_t gain_vol = (platform_getVolumeLevel()<<7) /20;
-            //int32_t t;
             int32_t avg = 0;
             audio_sample_t  peak = 0;
             audio_sample_t  sample_abs;
@@ -308,19 +307,14 @@ static void *decodeFunc(void *arg)
                 sample_abs = abs(audioBuf[i]);
                 if( sample_abs > peak)
                     peak =  sample_abs;
-                
                 avg += sample_abs;
-                
-               // t =  audioBuf[i] ;
                 audioBuf[i] = (audio_sample_t)( ((int32_t)audioBuf[i] * (int32_t)gain_vol) >> 7);
             }
             
             avg /= 160;
             
             if(avg < gateThresh)
-            {
-                for(size_t i = 0; i < 160; i++)
-                    audioBuf[i] = 0;
+                for(size_t i = 0; i < 160; i++) audioBuf[i] = 0;
             }
             
                /* 
